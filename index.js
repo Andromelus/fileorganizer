c23d9be0de0347f5544fc32f07837286f5d63db8
florianc <castelainflorian44@gmail.com>
2019-09-29 19:08:46 +0200

    [+++]  ADD SORT BY NAME DATE

--- index.js
+++ index.js
@@ -1,39 +1,96 @@
 const fs = require('fs');
 const path = require('path');
-const pathToFiles = process.argv[2];
+// namedate
+// mtime
+const sortType = process.argv[2];
+const pathToFiles = process.argv[3];
 
 console.log("Organizing files from " + pathToFiles);
 
 let files = fs.readdirSync(pathToFiles);
 
-const months = ['janvier', 'fevrier', 'mars', 'avril', 'mai', 'juin', 'juillet', 'aout', 'septembre', 'octobre', 'novembre', 'decembre'];
+const months = ['1 - janvier', '2 - fevrier', '3 - mars', '4 - avril', '5 - mai', '6 - juin', '7 - juillet', '8 - aout', '9 - septembre', '10 - octobre', '11 - novembre', '12 - decembre'];
 
-files.forEach(file => {
-    let fileCTime = (fs.statSync(path.join(pathToFiles, file))).mtime;
-    let fileCTimeYear = fileCTime.getFullYear();
-    let fileCTimeMonth = fileCTime.getMonth();
-    let currentPath = path.join(pathToFiles, file);
-    if(!fs.lstatSync(currentPath).isDirectory()){
-        let yearPath = path.join(pathToFiles, fileCTimeYear.toString());
-        let monthPath = path.join(yearPath, months[fileCTimeMonth]);
-        console.log(monthPath);
-        if (!fs.existsSync(yearPath)){
-            fs.mkdirSync(yearPath);
+
+if (sortType === "mtype") {
+    files.forEach(file => {
+        let times = getMtime(file);
+        let fileCTimeYear = times[0].toString();
+        let fileCTimeMonth = times[1];
+        let currentPath = path.join(pathToFiles, file);
+        if (!fs.lstatSync(currentPath).isDirectory()) {
+            let yearPath = path.join(pathToFiles, fileCTimeYear);
+            let monthPath = path.join(yearPath, months[fileCTimeMonth]);
+            console.log(monthPath);
+            if (!fs.existsSync(yearPath)) {
+                fs.mkdirSync(yearPath);
+            }
+            if (!fs.existsSync(monthPath)) {
+                fs.mkdirSync(monthPath);
+            }
+            move(currentPath, path.join(monthPath, file), function () {
+                console.log("Moved from " + currentPath + " to " + monthPath);
+            });
         }
-        if (!fs.existsSync(monthPath)){
-            fs.mkdirSync(monthPath);
+        else {
+            console.log('Is directory, ignoring');
         }
-        move(currentPath, path.join(monthPath, file), function(){
-            console.log("Moved from " + currentPath + " to " + monthPath);
-        });
+    });
+}
+
+else if (sortType === "namedate") {
+    files.forEach(file => {
+        let time = getTime(file);
+        let year = time[0].toString();
+        let month = time[1];
+        console.log(year + " " + month);
+        let currentPath = path.join(pathToFiles, file);
+        if (!fs.lstatSync(currentPath).isDirectory()) {
+            let yearPath = path.join(pathToFiles, year);
+            let monthPath = path.join(yearPath, months[month]);
+            if (!fs.existsSync(yearPath)) {
+                fs.mkdirSync(yearPath);
+            }
+            if (!fs.existsSync(monthPath)) {
+                fs.mkdirSync(monthPath);
+            }
+            move(currentPath, path.join(monthPath, file), function () {
+                console.log("Moved from " + currentPath + " to " + monthPath);
+            });
+        }
+        else {
+            console.log('Is directory, ignoring');
+        }
+    });
+}
+else {
+    console.log("sort type not supported");
+}
+
+function getTime(fileName) {
+    // Match "YYYYMMDD" in file name
+    var rePattern = new RegExp(/(20\d{2})(\d{2})(\d{2})/);
+    var result = fileName.match(rePattern);
+    console.log(fileName);
+    console.log( " is in name " + result);
+    if (result === null) {
+        console.log("Date is not in name, using mtime");
+        let times = getMtime(fileName);
+        return [times[0], times[1]];
     }
     else {
-        console.log('Is directory, ignoring');
+        let year = result[1];
+        let month = result[2];
+        return [year, parseInt(month) - 1];
     }
+}
 
-
-});
-
+function getMtime(fileName) {
+    let fileCTime = (fs.statSync(path.join(pathToFiles, fileName))).mtime;
+    let fileCTimeYear = fileCTime.getFullYear();
+    let fileCTimeMonth = fileCTime.getMonth();
+    return [fileCTimeYear, fileCTimeMonth];
+}
 
 function move(oldPath, newPath, callback) {
 
--- readme.md
+++ readme.md
@@ -1,19 +1,26 @@
 # Description
 
+-----
+
 **WARNING**
 
 Months are written in french.
 
-Sort files in folders based on year and month of the mtime (modification time) of the file.
+-----
+Sort files in folders based on year and month of the mtime (modification time) of the file OR based on the date (YYYYMMDD) in the file name.
 
 All the files should be in the very same folder.
 
 # How to use
 
+## Sort by mtime
 ```shell
-node index.js <pathofmyfiles>
+node index.js mtime <pathofmyfiles>
 ```
-
+## Sort by name date
+```shell
+node index.js namedate <pathofmyfiles>
+```
 # Example
 
 ## Before
